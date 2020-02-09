064d2993f28d0b786c9f83639fc45fa8113355ab
Yunfei Li <uryu.li3@gmail.com>
2020-02-08 21:21:24 -0500

    added migrations

--- BethanysPieShop/Models/Pie.cs
+++ BethanysPieShop/Models/Pie.cs
@@ -14,11 +14,13 @@ namespace BethanysPieShop.Models
         public string AllergyInformation { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
-        public string ImageThumbnaiUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
 
         public bool IsPieOfTheWeek { get; set; }
         public bool InStock { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
+
+        public string Notes { get; set; }
     }
 }
